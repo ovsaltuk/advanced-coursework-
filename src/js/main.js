5c3f912a9f33fd95c534bab8f5929c1230ea834d
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2023-05-14 13:22:58 +0300

    hero swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,22 @@
 import '../scss/main.scss';
 import '../index.html';
 import Dropdown from './modules/dropdown';
+import Swiper from 'swiper/bundle';
 
 new Dropdown('region');
 new Dropdown('category');
+
+const swiper = new Swiper('.js-hero__swiper', {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.hero__swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+});
