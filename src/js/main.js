13bc35dfbe7a1cae6c11c495c073442ca6fd07ed
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2023-05-15 15:36:46 +0300

    useful section slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,21 @@
 import '../scss/main.scss';
 import '../index.html';
-import Dropdown from './modules/dropdown';
+import Dropdown from './modules/Dropdown';
 import Swiper from 'swiper/bundle';
+import ShowMoreItems from './modules/ShowMoreItems';
 
 new Dropdown('region');
 new Dropdown('category');
 
+
+//swiper hero
 const heroSwiper = new Swiper('.js-hero__swiper', {
-  // Optional parameters
+
   loop: true,
   autoplay: {
     delay: 5000,
   },
-  // If we need pagination
+
   pagination: {
     el: '.hero__swiper-pagination',
     type: 'bullets',
@@ -20,6 +23,7 @@ const heroSwiper = new Swiper('.js-hero__swiper', {
   },
 });
 
+//swiper specail offers
 const swiperSpecialOffers = new Swiper('.special-offers-slider', {
   navigation: {
     nextEl: '.special-offers__swiper-button-next',
@@ -29,3 +33,18 @@ const swiperSpecialOffers = new Swiper('.special-offers-slider', {
   slidesPerGroup: 3,
   spaceBetween: 32,
 });
+
+//specialOffers
+const specialOfferShowMoreItems = new ShowMoreItems('high-rating', 'specialOfferBnt');
+
+//useful swiper 
+const usefulSwiper = new Swiper ('.useful__swiper', {
+  navigation: {
+    nextEl: '.js-useful-next',
+    prevEl: '.js-useful-prev',
+  },
+
+  slidesPerView: 'auto',
+  slidesPerGroup: 2,
+  spaceBetween: 32,
+})
